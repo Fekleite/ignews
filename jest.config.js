064113a40ec1cf7b22bdacdf3649cc25f042b8f7
Fernanda Leite <fernandacarolineleite360@gmail.com>
2021-05-30 00:00:20 -0300

    config: coverage tests

--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,13 @@ module.exports = {
   moduleNameMapper: {
     '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
   },
-  testEnvironment: "jsdom"
+  testEnvironment: "jsdom",
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "src/**/*.tsx",
+    "!src/**/*.spec.tsx",
+    "!src/**/*_app.tsx",
+    "!src/**/*_document.tsx",
+  ],
+  coverageReporters: ["json", "lcov"]
 };
